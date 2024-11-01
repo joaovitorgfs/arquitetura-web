f62e93fd59c40f94079532075b0cefc6e2f4b119
João Vitor <77232143+joaovitorgfs@users.noreply.github.com>
2024-11-01 20:36:30 -0300

    Atividade - JWT Rest API (AUTH TOKEN)

---
+++ Atividade 05 (JWT)/jwtrestapi/src/main/java/com/example/jwtrestapi/JwtrestapiApplication.java
@@ -0,0 +1,12 @@
+package com.example.jwtrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtrestapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtrestapiApplication.class, args);
+    }
+}
---
+++ Atividade 05 (JWT)/jwtrestapi/src/main/java/com/example/jwtrestapi/service/AuthService.java
@@ -0,0 +1,17 @@
+package com.example.jwtrestapi.service;
+
+import com.example.jwtrestapi.security.JwtUtil;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+    public String generateToken(String username) {
+        String token = JwtUtil.generateToken(username);
+        return token;
+    }
+
+    public String extractUsername(String token) {
+        String username = JwtUtil.extractUsername(token);
+        return username;
+    }
+}
